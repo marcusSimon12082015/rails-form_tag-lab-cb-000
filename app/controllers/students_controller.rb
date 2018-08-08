4b24801917229d04d8e2c7bf38d59f0c6b834078
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-08 18:09:28 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,7 @@ class StudentsController < ApplicationController
     Student.create!(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to new_student_path
   end
+
   def new
     @student = Student.last
   end
